5f3ec681645d7ae5fe692af00b362dc38c158b2f
rocioinnocenti <rocioinnocenti@gmail.com>
2025-05-28 15:15:03 +0200

    test_visual_homepage
    Added wait_for_timeout so that the snapshot of the homepage is the same every time (it is animated)

--- tests/test_visual_homepage.py
+++ tests/test_visual_homepage.py
@@ -3,4 +3,5 @@ from playwright.sync_api import Page, expect
 def test_visual_homepage(page:Page, assert_snapshot):
     print("Given user visit homepage")
     page.goto("https://www.accenture.com/es-es")
+    page.wait_for_timeout(3000)
     assert_snapshot(page.screenshot())

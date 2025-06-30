b47069d8a5e13f6345fa703a051f6c9b0c89c6c4
rocioinnocenti <rocioinnocenti@gmail.com>
2025-06-30 15:05:34 +0200

    Update test_visual_homepage.py

--- tests/test_visual_homepage.py
+++ tests/test_visual_homepage.py
@@ -4,7 +4,7 @@ import utils
 def test_visual_homepage(page:Page, assert_snapshot):
     print("Given user visit homepage")
     page.goto("https://www.accenture.com/es-es")
-    page.wait_for_timeout(3000)
+    page.wait_for_timeout(5000)
 
     if(utils.is_mobile(page)):
         print("Mobile detected, skipping screenshot")

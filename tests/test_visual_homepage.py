f0fbbb815a2f6dc01aaaf5a01f84df754750566d
rocioinnocenti <rocioinnocenti@gmail.com>
2025-06-30 15:33:06 +0200

    Remove test visual homepage

--- tests/test_visual_homepage.py
+++ tests/test_visual_homepage.py
@@ -1,14 +1,14 @@
 from playwright.sync_api import Page, expect
 import utils 
 
-def test_visual_homepage(page:Page, assert_snapshot):
-    print("Given user visit homepage")
-    page.goto("https://www.accenture.com/es-es")
-    page.wait_for_timeout(5000)
+#def test_visual_homepage(page:Page, assert_snapshot):
+ #   print("Given user visit homepage")
+  #  page.goto("https://www.accenture.com/es-es")
+   # page.wait_for_timeout(5000)
 
-    if(utils.is_mobile(page)):
-        print("Mobile detected, skipping screenshot")
-        return
-    else:
-        print("Desktop detected, taking screenshot")
-        assert_snapshot(page.screenshot())
+    #if(utils.is_mobile(page)):
+     #   print("Mobile detected, skipping screenshot")
+      #  return
+    #else:
+     #   print("Desktop detected, taking screenshot")
+      #  assert_snapshot(page.screenshot())
